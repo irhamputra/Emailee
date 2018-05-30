da5322493a1c07155940668184d5384aa045075c
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-30 16:30:47 +0200

    delete

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-// dont commit this
-module.exports = {
-    googleClientID: "368334320826-na2cdm8sqge5rvr1vqro3msjla6ii70l.apps.googleusercontent.com",
-    googleClientSecret: "yhhHeigMEa4_7hKpwflooxeY",
-    MongoUri: 'mongodb://emailee_DB:Mnemezius1991!@ds241530.mlab.com:41530/emailee',
-    cookieKey: 'dGhlIHF1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZw=='
-};
