52edb02b34f1201f6c21c303644ec0d2ff737700
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-30 11:33:15 +0200

    body parser, cors and mongoose dependencies

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 require('dotenv').config();
 
 const app = express();
+
+passport.use(new GoogleStrategy());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,6 +19,10 @@ const PORT = process.env.PORT || 8000;
 app.get('/', (req, res, next) => {
     res.send({ "hello": "World" });
     next();
+});
+
+app.use("/auth/google", (req, res, next) => {
+
 });
 
 app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
