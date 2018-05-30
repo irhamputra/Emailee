23a493ec339d7bc030f7cf3854b0a1b612396027
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-30 10:18:08 +0200

    cors and body-parser

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
 const PORT = 8001 || process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/', (req, res, next) => {
+    res.send({ "hello": "World" });
+    next();
 });
 
 app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
