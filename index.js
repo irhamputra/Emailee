91e6ec07b4a92247a1464e204260c0d76cc0cdb8
Muhamad Irham Prasetyo <irhamputraprasetyo@gmail.com>
2018-05-30 14:15:04 +0200

    Connect to MongoDB, Cookie Session and passport init

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 require('dotenv').config();
+require('./server/models/User');
 require('./server/services/passport');
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 2592000000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const MongoURI = keys.MongoUri;
+
+if (MongoURI) {
+    mongoose.connect(MongoURI);
+} else {
+    throw new Error('Please provide your MongoDB URI!')
+}
 
 require('./server/routers/auth')(app);
 
@@ -15,9 +37,8 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8001;
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     res.send({"hello": "World"});
-    next();
 });
 
 app.listen(PORT, () => console.log(`Server is running on localhost:${PORT}`));
